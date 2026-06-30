3dc21e2cfd482a2e8397178c875b150b50e0bd9a
Fabricio <fabriciomaidanaa14@gmail.com>
2026-06-30 16:15:27 -0300

    Correción leve

--- archivo.py
+++ archivo.py
@@ -1,18 +1,18 @@
 import json
 import os
- 
+
 RUTA = "alumnos.json"
- 
+
 def cargar_archivo() -> list:
     """Carga la lista de alumnos almacenada en el archivo JSON.
- 
+
     Args:
         Ninguno.
- 
+
     Returns:
         list: Lista de alumnos. Lista vacía si el archivo no existe.
     """
- 
+
     if os.path.exists(RUTA):
         f = open(RUTA, "r")
         alumnos = json.load(f)
@@ -22,15 +22,14 @@ def cargar_archivo() -> list:
  
 def guardar_archivo(alumnos: list) -> None:
     """Guarda la lista de alumnos en el archivo JSON.
- 
+
     Args:
         alumnos (list): Lista de alumnos a guardar.
- 
+
     Returns:
         None
     """
- 
+
     f = open(RUTA, "w")
     json.dump(alumnos, f, indent=4)
     f.close()
- 
